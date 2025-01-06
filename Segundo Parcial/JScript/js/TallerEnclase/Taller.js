01d8d6609ec03e1daa454c892e2c6f58d0d6b648
SebasParra <sebasraster10@gmail.com>
2025-01-06 10:33:57 -0500

    Actualizacion taller

--- Segundo Parcial/JScript/js/TallerEnclase/Taller.js
+++ Segundo Parcial/JScript/js/TallerEnclase/Taller.js
@@ -1,10 +1,17 @@
 function calcularPromedio(a, b, c){
+    if(typeof a === 'number' && typeof b === 'number' && typeof c === 'number'){
     return (a+b+c)/3;
+    }
+    else{
+        console.log("Alguno de los aprametros no es un numero");
+    }
 }
 
-const determinarMayor = function(a, b){return a > b ? a : b}
+const determinarMayor = function(a, b){if(typeof a === 'number' && typeof b === 'number'){  return a > b ? a : b} else{
+    console.log("Alguno de los aprametros no es un numero");
+}}
 
-const esPar = (a) => a % 2 == 0 ? true : false;
+const esPar = (a) => a % 2 == 0 && typeof a === 'number' ? true : false;
 
 (function(){
     console.log(calcularPromedio(18, 20, 13));
