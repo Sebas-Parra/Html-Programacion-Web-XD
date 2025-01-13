eb42a5d28c22a1dbb60bcfcb07188befd7535f28
SebasParra <sebasraster10@gmail.com>
2025-01-13 10:07:53 -0500

    Prueba

--- Segundo Parcial/JScript/js/Prueba1/Prueba.js
+++ Segundo Parcial/JScript/js/Prueba1/Prueba.js
@@ -1,4 +1,4 @@
-class Producto{
+class Producto{ //CLase producto
     constructor(Nombre, precio, cantidad, categoria){
         this.Nombre = Nombre;
         this.precio = precio;
@@ -7,14 +7,14 @@ class Producto{
     }
 }
 
-class Inventario{
+class Inventario{ //Clase inventario
     PrDucts = [];
 
-    AgregarProducto(Nombre, precio, cantidad, categoria){
+    AgregarProducto(Nombre, precio, cantidad, categoria){ //Agrega producto al inventario de manera dinamica
         this.PrDucts.push(new Producto(Nombre, precio, cantidad, categoria));
     }
 
-    FiltrarCategoria(categoria){
+    FiltrarCategoria(categoria){ //Filtra productos por categoria
         for(let producto of this.PrDucts){
             if(producto.categoria == categoria){
                 console.log(producto);
@@ -22,7 +22,7 @@ class Inventario{
         }
     }
 
-    Descuento(categoria, porcentaje){
+    Descuento(categoria, porcentaje){ //Aplica descuento a todos los productos de una categoria
         for(let producto of this.PrDucts){
             if(producto.categoria == categoria){
                 producto.precio -= producto.precio * (porcentaje / 100);
@@ -35,13 +35,13 @@ class Inventario{
 
 class Venta{
     ventas = [];
-    realizarVenta(Nombre, cantidad){
+    realizarVenta(Nombre, cantidad){ //Realiza la venta del producto del inventario
         for(let producto of invent.PrDucts){
-            if(producto.Nombre == Nombre){
-                if(producto.cantidad >= cantidad){
+            if(producto.Nombre == Nombre){ //Verifica si el nombre del producto existe en el inventario
+                if(producto.cantidad >= cantidad){ //Verifica si hay suficientes productos en el stock
                     producto.cantidad -= cantidad;
                     console.log('Venta realizada del producto ');
-                    this.ventas.push(new Producto(producto.Nombre, producto.precio, producto.cantidad, producto.categoria));
+                    this.ventas.push(new Producto(producto.Nombre, producto.precio, cantidad, producto.categoria));
                 }else{
                     console.log('NO hay suficientes productos en el stock');
                 }
@@ -57,23 +57,26 @@ class Venta{
 
 }
 
-
+//Agregacion de productos al inventario
 let invent = new Inventario();
 console.log('--------------------PRODUCTOS EN EL INVENTARIO---------------------');
 invent.AgregarProducto('Cola', 1, 10, 'Refresco');
 invent.AgregarProducto('Papas', 3, 15, 'Snacks');
 invent.AgregarProducto('Doritos', 2, 20, 'Snacks');
+invent.AgregarProducto('Celular', 100, 5, 'Electronicos');
+invent.AgregarProducto('Cueritos', 2, 30, 'Snacks');
 console.log('--------------------CATEGORIAS FILTRADAS---------------------');
 invent.FiltrarCategoria('Snacks');
 console.log('--------------------DESCUENTOS---------------------');
 invent.Descuento('Snacks', 10)
-
+invent.Descuento('Electronicos', 20)
 console.log('----------------VENTAS---------------------');
 
 let venta = new Venta();
 venta.realizarVenta('Cola', 3);
 venta.realizarVenta('Papas',5);
 venta.realizarVenta('Doritos', 6);
+venta.realizarVenta('Celular', 2);
 venta.MostrarVentas();
 
 console.log('');
@@ -104,7 +107,7 @@ for(let vent of venta.ventas){
         masVendido = vent;
     }
 }
-console.log('Producto que mas se vendió: '+ masVendido.Nombre + 'con ' + masVendido.cantidad + 'unidades vendidas');
+console.log('Producto que mas se vendió: '+ masVendido.Nombre + ' con ' + masVendido.cantidad + ' unidades vendidas');
 console.log('-----------------------------------------------------------------------------------------------------------');
 
 
